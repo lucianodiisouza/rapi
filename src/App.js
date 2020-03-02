a51a5210c1a58a58c334a34f3ee08cfd72893f53
Luciano dii Souza <contato@thecodelovers.com.br>
2020-03-02 20:31:34 -0300

    Initial commmit with some customizations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './Estilos.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Carregando...</div>
+    } else {
+      return (
+        <section className="container">
+          <h2><center>Listing Data from JSON</center></h2>
+          <br />
+          <section className="lista">
+            {items.map(item => (
+              <p className="item" key={item.id}>
+                <span className="nome">Nome: {item.name} </span>
+                <span className="email">Email: {item.email} </span>
+                <span className="empresa"> Empresa: {item.company.name}</span>
+                <span><a href={ 'http://'+item.website } target="_blank">Click here to Access the WebSite</a></span>
+              </p>
+            ))}
+          </section>
+        </section>
+
+      );
+    }
+  }
 }
+
 
 export default App;
